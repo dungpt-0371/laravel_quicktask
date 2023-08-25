d3f0ea776ce88648cf205533af6144f8eac5d5e2
Arima057nn <tjendung97pain@gmail.com>
2023-08-25 16:41:34 +0700

    Task 3: Tạo relationships trong models

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
@@ -12,4 +13,9 @@ class Task extends Model
     protected $table = 'tasks';
 
     protected $fillable = ['content'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
